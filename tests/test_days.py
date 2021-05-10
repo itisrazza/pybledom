b2acf798bf16eae837ae3f21106e91a90cc2cbae
Razz Nistor <raresh@nistor.email>
2021-05-10 13:05:56 +1200

    write some tests (so the pipeline doesn't fail)

---
+++ tests/test_days.py
@@ -0,0 +1,23 @@
+from bledom import Days
+
+
+def test_all_days():
+    assert Days.ALL == (Days.MONDAY |
+                        Days.TUESDAY |
+                        Days.WEDNESDAY |
+                        Days.THURSDAY |
+                        Days.FRIDAY |
+                        Days.SATURDAY |
+                        Days.SUNDAY)
+
+
+def test_weekend_days():
+    assert Days.WEEKEND_DAYS == (Days.SATURDAY | Days.SUNDAY)
+
+
+def test_week_days():
+    assert Days.WEEK_DAYS == (Days.MONDAY |
+                              Days.TUESDAY |
+                              Days.WEDNESDAY |
+                              Days.THURSDAY |
+                              Days.FRIDAY)
